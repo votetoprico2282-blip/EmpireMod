0ee745f770abd762af8633b8f417e3f59f8db983
votetoprico2282-blip <votetoprico228.2@gmail.com>
2026-03-21 12:41:19 +0300

    Update EnderSpearItem.java

--- EnderSpearItem.java
+++ EnderSpearItem.java
@@ -1,9 +1,26 @@
-package ru.terrashade.empire.item;
+package com.yourname.enderspear;
 
-import net.minecraft.item.TridentItem;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterials;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.minecraft.util.Hand;
 
-public class EnderSpearItem extends TridentItem {
-    public EnderSpearItem(Settings settings) {
-        super(settings);
+public class EnderSpear implements ModInitializer {
+
+    // Создаем само копье (база от меча для урона)
+    public static final Item ENDER_SPEAR = new SwordItem(ToolMaterials.NETHERITE, 5, -2.4f, new Item.Settings().maxCount(1));
+
+    @Override
+    public void onInitialize() {
+        // Регистрируем предмет на сервере
+        Registry.register(Registries.ITEM, new Identifier("endersmp", "ender_spear"), ENDER_SPEAR);
     }
 }
